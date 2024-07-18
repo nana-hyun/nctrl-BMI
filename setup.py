1132c5496b863ad784fc8ec3e5895ae24ec6a832
NaMoo <nahyunatoz@gmail.com>
2024-07-18 16:45:04 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
-setup(name='nctrl-BMI',
-        version='0.0.1',
-        description='realtime BMI with spiketag',
-        url='https://github.com/nana-hyun/nctrl-BMI',
-        author='Nahyun Kim',
-        packages=['nctrl-BMI']
-        )
+setup(
+    name="nctrl-bmi",
+    version="0.0.1",
+    description="realtime BMI with spiketag",
+    url="https://github.com/lapis42/nctrl-bmi",
+    author="Nahyun Kim, Dohoung Kim",
+    packages=["nctrl"],
+)
